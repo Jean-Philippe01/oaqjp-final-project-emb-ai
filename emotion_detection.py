28d755d57b00e57833f5d79da00d182048830109
Jean-Philippe01 <jeanphilipp.neris@gmail.com>
2025-02-09 22:34:12 -0500

    Changed the emotion_detector() function so that it only returns the emotions with their scores and the dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -7,4 +8,17 @@ def emotion_detector(text_to_analyse):
 
     response = requests.post(URL, json=input_json, headers=headers)
 
-    return response.text
+    # convert json to dict:
+    emo_dict = json.loads(response.text)
+
+    # new_emo_dict is created to isolate the emotions with their scores
+    # {'anger': 0.0132405795, 'disgust': 0.0020517302, 'fear': 0.009090992, 'joy': 0.9699522, 'sadness': 0.054984167}
+    new_emo_dict = emo_dict["emotionPredictions"][0]["emotion"]
+
+    # Select the emotion with the dominant score from new_emo_dict
+    dominant_emotion = max(new_emo_dict, key=new_emo_dict.get)
+
+    # Add dominant_score to new_emo_dict
+    new_emo_dict['dominant_emotion'] = dominant_emotion
+    
+    return new_emo_dict
